881f79059abcc12e67032a04fe71b6a0e139410e
Kamikaze2942 <jadhya2006@gmail.com>
2025-10-08 17:46:28 +0200

    Jeet-Menu

---
+++ Menu.c
@@ -0,0 +1,16 @@
+int OP;
+
+do
+{
+    printf("Enter 1 or 2 for the operation: /n");
+    scanf("%d", &OP);
+} while(OP > 2 || OP < 1)
+
+if (OP == 1)
+{
+	printf("%d * %d = %d\n" = V1, V2,V1 + V2);
+}
+else
+{
+	printf("%d * %d = %d\n" = V1, V2,V1 * V2);
+}
