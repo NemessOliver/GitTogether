2306ebc3ea39298212f3008e4d230e4ce29d7168
Larry Lan <larry.lan@student.nhlstenden.com>
2025-10-08 10:57:33 +0200

    blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+void blink();
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// Turn the built-in LED on for 1 s, then off for 1 s.
+void blink() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
