1329eff10aae591d69158b50b9074feb738e69bd
Kamikaze2942 <jadhya2006@gmail.com>
2025-10-10 10:10:18 +0200

    menu fixed

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,23 @@ void setup() {
   // put your setup code here, to run once:
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  int OP;
+
+  do {
+    printf("Enter 1 or 2 for the operation: /n");
+    scanf("%d", &OP);
+  } while (OP > 2 || OP < 1);
+
+    if (OP == 1) {
+    return;
+  }
+  else {
+    return;
+  }
 }
+
 
 // turn the built-in LED on for 1s, then off for 1s
 void blink() {
